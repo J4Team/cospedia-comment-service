9cee788fcf5b95ee7dd5683fef968cec2aab28e0
sevdadurdu <sevdadurdu@outlook.com>
2018-08-09 15:29:20 +0300

    Initial commit

---
+++ src/main/java/com/cospedia/commentservice/CospediaCommentServiceApplication.java
@@ -0,0 +1,12 @@
+package com.cospedia.commentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CospediaCommentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CospediaCommentServiceApplication.class, args);
+	}
+}
---
+++ src/test/java/com/cospedia/commentservice/CospediaCommentServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cospedia.commentservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CospediaCommentServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
